4d711664d5a66760b24134f8a35a16571cbad267
Alexey Zolotykh <Alexey.Zolotykh@infobip.com>
2020-07-31 13:08:39 +1200

    Updates

--- lelya.e31bb0bc.js
+++ lelya.e31bb0bc.js
@@ -176,7 +176,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60934" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64809" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
